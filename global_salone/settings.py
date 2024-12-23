63123b8d194e1b18eb3ddb2a2a891edb748360ab
scarecrow <137720412+chernor626@users.noreply.github.com>
2024-12-23 23:23:23 +0000

    update

--- global_salone/settings.py
+++ global_salone/settings.py
@@ -104,11 +104,14 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'glabalsalone_db',         # Name of your new database
+        'USER': 'glabalsalone_db',        # Your MySQL username
+        'PASSWORD': 'Corn626@',           # Your MySQL password
+        'HOST': 'localhost',              # Database host (usually localhost)
+        'PORT': '3306',                   # MySQL default port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
